e9cd18d92a7073db9dd195e81a8c0e2f481b12d7
sw6363 <61794408+sw6363@users.noreply.github.com>
2020-05-21 02:34:34 +0900

    basic

---
+++ for.py
@@ -0,0 +1,55 @@
+for num in [0,1,2]:
+    print(num)
+
+characters=['엘리스','도도새','3월토끼']
+for character in characters:
+    print(character)
+
+players=['공작부인','흰 토끼','하트잭','모자장수']
+for player in players:
+    print(player,'퇴장!')
+
+for letter in '체셔고양이':
+    print(letter)
+
+nums=[0,1,2]
+for num in nums:
+    print(num)
+print(nums)
+
+for num in nums:
+    print(num)
+    print(nums)
+
+for num in range(3):
+    print(num)
+
+for y in range(1,10):
+    print(2,'x',y,'=',2*y)
+
+roses=['하연장미','하얀장미','하얀장미']
+for i in range(3):
+    roses[i]='빨간장미'
+print(roses)
+
+clovers=['클로버1','클로버2','클로버3']
+for clover in clovers:
+    print(clover)
+
+clovers=['클로버1','클로버2','클로버3']
+for i in range(3):
+    print(clovers[i])
+
+for num in range(1,4):
+	print('*'*num)
+
+stars=[2,1,3]
+for num in stars:
+    print('*'*num)
+
+total=0
+card_nums=[1,3,6,7]
+for num in card_nums:
+    total=total+num
+print(total/4)
+
